f8561a5b8d6e215009e5885636cc76b0387bba6c
xxr5862 <xxr5862@g.rit.edu>
2025-07-23 14:57:56 -0400

    finished

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,6 +1,6 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"review" : "okay"}, "isbn": "1234567890" },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {"review" : "okay"} },
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": [], "isbn": "1234567890" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": [] },
       3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {"review" : "okay"} },
       4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {"review" : "okay"} },
       5: {"author": "Unknown","title": "The Book Of Job", "reviews": {"review" : "okay"} },
